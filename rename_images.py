def81370a8fbf9fd4cc51a1d46f7ec1e5ed686bb
Daniel-Siozis <167128660+Daniel-Siozis@users.noreply.github.com>
2024-06-08 16:50:09 +0200

    Update rename_images.py

--- rename_images.py
+++ rename_images.py
@@ -1,7 +1,9 @@
 import os
 import shutil
+from PIL import Image
 
-def copy_rename_images(source_dir, dest_dir):
+def rescale_images(source_dir, dest_dir):
+    newSize = (64,64)
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
     counter = 1
@@ -16,9 +18,12 @@ def copy_rename_images(source_dir, dest_dir):
             dest_file = os.path.join(dest_dir, new_filename)
             # Kopiere die Datei in das Zielverzeichnis und benenne sie dabei um
             shutil.copy(source_file, dest_file)
+            with Image.open(dest_file) as img:
+                resized_img = img.resize(newSize, Image.Resampling.LANCZOS)
+                resized_img.save(dest_file)
             counter = counter +1
 
 # Beispielaufruf der Funktion
-source_directory = "Dataset\originial"
+source_directory = "Dataset\original"
 destination_directory = "Dataset\lid"
-copy_rename_images(source_directory, destination_directory)
+rescale_images(source_directory, destination_directory)
