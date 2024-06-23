ab0a7e2a1d2942703352a5821ef2d69e33f9d682
LilBabyGoat <167232698+LilBabyGoat@users.noreply.github.com>
2024-06-23 15:52:16 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 from PIL import Image
 from tensorflow.keras.models import load_model
 
-# CSS für den animierten Hintergrund und spezifische Stile
 page_bg_img = """
 <style>
 [data-testid="stAppViewContainer"] {
@@ -33,21 +32,24 @@ h1, h2, h3, h4, h5, h6 {
     font-family: 'Lato', sans-serif;
     color: #000;
     padding: 10px;
-    border: none;  /* Kein Rahmen */
-    margin: 0;     /* Kein äußerer Abstand */
+    border: none;
+    margin: 0;
     border-radius: 5px;
-    position: fixed;  /* Fixiere das Element */
-    top: 0;           /* Abstand vom oberen Rand */
-    left: 0;          /* Abstand vom linken Rand */
-    width: 100%;      /* Volle Breite */
-    z-index: 1000;    /* Über anderen Elementen */
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 1000;
 }
 
 .info-box {
-    background-color: #FA0707;
+    background-color: #FFDE59;
     color: #FFFFFF;
     border-radius: 5px;
     padding: 10px;
+}
+.example {
+  color: #FFFFFF;
 }
 
 .fische {
@@ -56,29 +58,67 @@ h1, h2, h3, h4, h5, h6 {
     padding: 10px;
     border-radius: 5px;
 }
+
+/* Sidebar-Styling */
+[data-testid="stSidebar"] {
+    background-color: #262527;
+    width: 10% !important;
+}
+
+
+[data-testid="stSidebar"] h1, [data-testid="stSidebar"] h2, [data-testid="stSidebar"] h3, [data-testid="stSidebar"] h4, [data-testid="stSidebar"] h5, [data-testid="stSidebar"] h6 {
+    color: #FFFFFF;
+}
+
+[data-testid="stSidebar"] .css-1d391kg {
+    color: #FF0000;
+}
+[data-testid="stSidebar"] .st-emotion-cache-1nm2qww {
+    color: #FFFFFF;
+}
+[data-testid="stSidebar"] .st-emotion-cache-j6qv4b p {
+    color: #FFFFFF;
+}
+[data-testid="stSidebar"] .st-emotion-cache-ue6h4q{
+ color: #262527;
+}
+
+[data-testid="stSidebar"] .css-1l0t84s {
+    color: #FF0000;
+}
+
+[data-testid="stSidebar"] .st-ay {
+    background-color: #000080;
+    color: #FF0000;
+    }
+
+[data-testid="stSidebar"] .css-1cpxqw2 {
+    color: #FF0000;
+}
 </style>
 """
 
-# Einfügen des CSS in die Streamlit-App
 st.markdown(page_bg_img, unsafe_allow_html=True)
+st.sidebar.title("Menü")
+page = st.sidebar.radio("Selection-Screen", ["Hauptmenü", "Dokumentation", "Examples", "Über uns"])
 
-# Streamlit-Anwendung
-st.markdown('<div class="head"><h4>Sehen Sie zu wie sich ihr Bild eigenständig reparieren lässt!</h4></div>', unsafe_allow_html=True)
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.markdown('<div class="info-box">Information! Es dürfen nur Bilder verwendet werden, die unter Wasser aufgenommen wurden</div>', unsafe_allow_html=True)
-st.write("")
-# Datei hochladen
-uploaded_file = st.file_uploader("Laden Sie hier Ihre Fischbilder hoch!", type=["png", "jpg", "jpeg"])
 
-# Lade das trainierte Modell
+# Leere Sidebar, um sicherzustellen, dass die Sidebar sichtbar ist
+st.sidebar.empty()
+# Dann fügen wir den eigentlichen Inhalt hinzu
+if page != "Über uns":
+    st.markdown('<div class="head"><h4>Sehen Sie zu wie sich ihr Bild eigenständig reparieren lässt!</h4></div>', unsafe_allow_html=True)
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.markdown('<div class="info-box">Information! Es dürfen nur Bilder verwendet werden, die unter Wasser aufgenommen wurden.</div>', unsafe_allow_html=True)
+    st.write("")
+    uploaded_file = st.file_uploader("Laden Sie hier Ihre Fischbilder hoch!", type=["png", "jpg", "jpeg"])
+
 model = load_model('trained_model.h5')
-
-# Funktion zur Reparatur des Bildes
 def repair_image(image, target_size=(64, 64)):
     # Originalbildgröße speichern
     original_size = image.size
@@ -101,7 +141,7 @@ def repair_image(image, target_size=(64, 64)):
     return predicted_image
 
 # Wenn eine Datei hochgeladen wurde
-if uploaded_file is not None:
+if page!="Über uns" and uploaded_file is not None:
     # Zeige das hochgeladene Bild an
     st.image(uploaded_file, caption='Hochgeladenes Bild', use_column_width=True)
     
@@ -115,3 +155,73 @@ if uploaded_file is not None:
         
         # Zeige das reparierte Bild an
         st.image(repaired_image, caption='Repariertes Bild', use_column_width=True)
+
+
+if page == "Bild reparieren":
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption='Hochgeladenes Bild', use_column_width=True)
+        if st.button('Reparieren'):
+            image = Image.open(uploaded_file)
+            repaired_image = repair_image(image)
+            st.image(repaired_image, caption='Repariertes Bild', use_column_width=True)
+
+elif page == "Dokumentation":
+    st.write("Hier können weitere Unterpunkte hinzugefügt werden.")
+
+elif page == "Examples":
+    
+    
+    # Beispielbilder laden (entweder lokal oder von einer URL)
+    example_images = [
+        ("Beispielbild 1", "MarineInpaint/Bilder/oken/1-broken.jpg", "MarineInpaint/Bilder/original/7393_F1_f000000.jpg"),
+        
+    ]
+    
+    for title, original, repaired in example_images:
+        st.markdown('<div class="example">Hier sehen Sie wie es funktionieren kann</div>', unsafe_allow_html=True)
+        st.write(title)
+        st.image(original, caption='Originalbild', use_column_width=True)
+        st.image(repaired, caption='Repariertes Bild', use_column_width=True)
+        st.write("")
+
+elif page == "Über uns":
+    st.title("Über uns")
+    st.subheader("Unser Team")
+    box_style = """
+    <style>
+    .box {
+        background-color: #f9f9f9;
+        border: solid #ddd;
+        padding: 10px;
+        border-radius: 1px;
+        margin-bottom: 10px;
+    }
+    .box h3{
+    color: #000000; }
+    </style>
+    """
+    st.markdown(box_style, unsafe_allow_html=True)
+
+    st.markdown("""
+    <div class="box">
+        <h3>Daniel Siozis</h3>
+        <p>Modell Entwicklung</p>
+        <p><strong>Persona:</strong> Durch die Automatisierung komplexer Aufgaben kann KI die Effizienz und Produktivität in verschiedensten Branchen, von der Medizin bis zur Automobilindustrie, enorm steigern.
+                Durch die Automatisierung komplexer Aufgaben kann KI die Effizienz und Produktivität in verschiedensten Branchen, von der Medizin bis zur Automobilindustrie, enorm steigern.
+            Durch die enorme und schnelle Datenverarbeitung werden auch schnell neue Erkenntnisse gewonnen, die überall neue Erfolge erzielen könnten.
+ 
+</p>
+    </div>
+    """, unsafe_allow_html=True)
+    st.markdown("""
+    <div class="box">
+        <h3>Samuel Arapoglu</h3>
+        <p>Webentwicklung</p>
+        <p><strong>Persona:</strong> Durch meine Position als Werkstudent in der Energiewirtschaft habe ich neben der Tätigkeit als SAP ABAP Entwickler die Möglichekit bei der Implementierung eines internen GPT Modells 
+            mitzuwirken. KI kann dadurch wichtige Daten vor Dritten schützen.
+   </p>
+    </div>
+    """, unsafe_allow_html=True)
+    # Optional: Hinzufügen von Abständen für eine bessere Lesbarkeit
+    st.markdown("---")
+    
