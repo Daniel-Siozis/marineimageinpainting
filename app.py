adab2b0a143f7da10d931c37d0dbc4d5dd32e21c
Daniel-Siozis <167128660+Daniel-Siozis@users.noreply.github.com>
2024-06-25 20:45:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ h1, h2, h3, h4, h5, h6 {
 [data-testid="stSidebar"] .st-emotion-cache-ue6h4q{
  color: #262527;
 }
-[data-testid="caption"] .etr89bj0{
+[data-testid="caption"] .css-1b0udgb .etr89bj0{
  color: #FFFFFF !important;
 }
 [data-testid="collapsedControl"] .css-fblp2m{
