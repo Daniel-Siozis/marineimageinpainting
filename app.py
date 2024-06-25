9fea913679f1823a89db7416f4945afc6c824ad1
Daniel-Siozis <167128660+Daniel-Siozis@users.noreply.github.com>
2024-06-25 20:52:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -204,12 +204,12 @@ elif page == "Dokumentation":
     """
     st.markdown(box_style, unsafe_allow_html=True)
     st.markdown("""
-    <div class="box"><h3>1. Model</h3>Wir haben ein einfaches CNN-Modell (Convolutional Neural Network) für unser Marinebild-Inpainting-Projekt verwendet. Das Modell besteht aus mehreren Faltungsschichten für die Merkmalsextraktion und einer transponierten Faltungsschicht für die Rekonstruktion der reparierten Bilder.")</p></div> """, unsafe_allow_html=True)
+    <div class="box"><h3>1. Model</h3>Wir haben ein einfaches CNN-Modell (Convolutional Neural Network) für unser Marinebild-Inpainting-Projekt verwendet. Das Modell besteht aus mehreren Faltungsschichten für die Merkmalsextraktion und einer transponierten Faltungsschicht für die Rekonstruktion der reparierten Bilder.</p></div> """, unsafe_allow_html=True)
     st.markdown("""
     <div class="box"><h3>2. Training</h3>Kommt noch</p></div> """, unsafe_allow_html=True)
 
     st.markdown("""
-    <div class="box"><h3>3. Limitations</h3>One significant limitation we encountered was hardware constraints, particularly with GPU memory (VRAM) and RAM. This limitation restricted our ability to process high-resolution images, necessitating the downscaling of our underwater images to 64x64 pixels</p></div> """, unsafe_allow_html=True)
+    <div class="box"><h3>3. Limitations</h3>Eine wesentliche Einschränkung, auf die wir gestoßen sind, waren Hardwarebegrenzungen, insbesondere beim GPU-Speicher (VRAM) und RAM. Diese Einschränkung beschränkte unsere Fähigkeit, hochauflösende Bilder zu verarbeiten, und machte es erforderlich, unsere Unterwasserbilder auf 64x64 Pixel herunterzuskalieren.</p></div> """, unsafe_allow_html=True)
 
 
 
